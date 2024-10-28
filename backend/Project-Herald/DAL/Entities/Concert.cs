da11fbd23335e85ae68c7edc007ccf51d7b7fec6
halkszavu <halkszavu@outlook.com>
2024-10-28 17:18:27 +0100

    concert filled

--- backend/Project-Herald/DAL/Entities/Concert.cs
+++ backend/Project-Herald/DAL/Entities/Concert.cs
@@ -8,11 +8,11 @@ namespace DAL.Entities
 {
 	public class Concert
 	{
-        public string ID { get; set; }
-        // Location of the concert
-        // Time of the concert (a DateTime of the location)
-        // Programme of the concert (a list of pieces)
-        // Performer of the concert (a list of individual musicians or orchestras)
-        // The conductor of the concert
-    }
+		public string ID { get; set; }
+		// Location of the concert
+		// Time of the concert (a DateTime of the location)
+		// Programme of the concert (a list of pieces)
+		// Performer of the concert (a list of individual musicians or orchestras)
+		// The conductor of the concert
+	}
 }
