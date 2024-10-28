69bace02c185fdbd387ea3dfa044a726b289373c
halkszavu <halkszavu@outlook.com>
2024-10-28 16:16:33 +0100

    concert added

---
+++ backend/Project-Herald/DAL/Entities/Concert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+	public class Concert
+	{
+        public string ID { get; set; }
+        // Location of the concert
+        // Time of the concert (a DateTime of the location)
+        // Programme of the concert (a list of pieces)
+        // Performer of the concert (a list of individual musicians or orchestras)
+        // The conductor of the concert
+    }
+}
