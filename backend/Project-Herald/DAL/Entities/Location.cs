cd7023f60408240c8f2da498a94f7f207f766d2c
halkszavu <halkszavu@outlook.com>
2024-10-29 17:25:23 +0100

    required fields annotated

--- backend/Project-Herald/DAL/Entities/Location.cs
+++ backend/Project-Herald/DAL/Entities/Location.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,10 @@ namespace DAL.Entities
 	{
 		public string ID { get; set; }
 		// Name of the location
+		[Required]
 		public string Name { get; set; }
 		// Address of the location
+		[Required]
 		public string Address { get; set; }
 		// Description of the location
 		public string Description { get; set; }
