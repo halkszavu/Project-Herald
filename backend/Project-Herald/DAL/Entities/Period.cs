ca5bc0b872eac38392b1fad7929b988fe0c067d9
halkszavu <halkszavu@outlook.com>
2024-10-29 09:53:35 +0100

    Period added

---
+++ backend/Project-Herald/DAL/Entities/Period.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+	public class Period
+	{
+		public string ID { get; set; }
+		// Name of the era
+		// Description of the era
+	}
+}
