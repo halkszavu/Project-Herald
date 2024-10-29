2b341b333dd14161fff5c30f1c86cb90a2ea83c3
halkszavu <halkszavu@outlook.com>
2024-10-29 19:44:25 +0100

    added Services and started the IConcertService

---
+++ backend/Project-Herald/Bll/Services/IConcertService.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bll.Services
+{
+	public interface IConcertService
+	{
+		/// <summary>
+		/// Inserts a concert into the database
+		/// </summary>
+		/// <param name="concert">The concert to be inserted</param>
+		/// <returns>The inserted (finalized) concert record</returns>
+		Task<Concert> CreateConcertAsync(Concert concert);
+
+		Task<Concert> GetConcertAsync(string concertId);
+
+		Task<IEnumerable<Concert>> GetConcertsByLocation(Location location);
+	}
+}
