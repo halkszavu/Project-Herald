60be5bf280c02f9f23fd0545bef86606dada6e27
halkszavu <halkszavu@outlook.com>
2024-10-29 09:54:46 +0100

    genre added

---
+++ backend/Project-Herald/DAL/Entities/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+	public class Genre
+	{
+		public string ID { get; set; }
+		// Name of the genre
+		public string Name { get; set; }
+		// Description of the genre
+		public string Description { get; set; }
+	}
+}
