3f1d6ef9cc644b99369fba86ce1eed9263093118
halkszavu <halkszavu@outlook.com>
2024-10-31 00:22:28 +0100

    added ILocationService contract

---
+++ backend/Project-Herald/Bll/Services/ILocationService.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bll.Services
+{
+	public interface ILocationService
+	{
+		/// <summary>
+		/// Inserts a location into the database
+		/// </summary>
+		/// <param name="location">The location to be inserted</param>
+		/// <returns>The inserted (finalized) location record</returns>
+		Task<Location> CreateLocationAsync(Location location);
+		 										
+		Task<Location> GetLocationAsync(string locationId);
+
+		Task<IEnumerable<Location>> GetLocationsByName(string name);
+	}
+}
