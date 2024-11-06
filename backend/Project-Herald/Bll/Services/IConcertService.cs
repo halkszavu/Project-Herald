d16005b6571b77ce59a95c729518086320badbf0
halkszavu <halkszavu@outlook.com>
2024-11-06 11:38:23 +0100

    updated the IService

--- backend/Project-Herald/Bll/Services/IConcertService.cs
+++ backend/Project-Herald/Bll/Services/IConcertService.cs
@@ -16,10 +16,47 @@ namespace Bll.Services
 		/// <returns>The inserted (finalized) concert record</returns>
 		Task<Concert> CreateConcertAsync(Concert concert);
 
-		Task<Concert> GetConcertAsync(string concertId);
+        /// <summary>
+		/// Gets a specific concert by its ID
+		/// </summary>
+		/// <param name="concertId">The ID of the concert</param>
+		/// <returns>The full concert with the specified ID</returns>
+        Task<Concert> GetConcertAsync(string concertId);
 
-		Task<IEnumerable<Concert>> GetConcertsByLocation(Location location);
+		/// <summary>
+		/// Gets all concerts in a given location
+		/// </summary>
+		/// <param name="location">The location</param>
+		/// <returns>A collection of concerts that take space in the specified location</returns>
+		Task<IEnumerable<Concert>> GetConcertsByLocationAsync(Location location);
 
-		Task<IEnumerable<Concert>> GetConcertsBetweenDates(DateTime startDate, DateTime endDate);
-	}
+        /// <summary>
+        /// Gets all concerts that take place between two dates
+        /// </summary>
+        /// <param name="startDate">The earliest date the concert can start.</param>
+        /// <param name="endDate">The latest date the concert can start</param>
+        /// <returns>A collection of concerts</returns>
+        Task<IEnumerable<Concert>> GetConcertsBetweenDatesAsync(DateTime startDate, DateTime endDate);
+
+        /// <summary>
+        /// Gets all concerts that feature a specific orchestra
+        /// </summary>
+        /// <param name="orchestra">The specified orchestra</param>
+        /// <returns>A collection of concerts</returns>
+        Task<IEnumerable<Concert>> GetConcertsByOrchestraAsync(Orchestra orchestra);
+
+        /// <summary>
+        /// Gets all concerts that feature a specific genre
+        /// </summary>
+        /// <param name="genre">The specified genre</param>
+        /// <returns>A collection of concerts</returns>
+        Task<IEnumerable<Concert>> GetConcertsByGenreAsync(Genre genre);
+
+        /// <summary>
+        /// Gets all concerts that feature a given period (era)
+        /// </summary>
+        /// <param name="period">The specified period</param>
+        /// <returns>A collection of concerts</returns>
+		Task<IEnumerable<Concert>> GetConcertsByPeriodAsync(Period period);
+    }
 }
