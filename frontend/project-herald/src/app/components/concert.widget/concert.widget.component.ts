0f6f2c0678abdb720a04bf6260974d343e6b8fff
halkszavu <halkszavu@outlook.com>
2024-11-10 10:30:14 +0100

    update concert widget to display concert name and integrate with main page

--- frontend/project-herald/src/app/components/concert.widget/concert.widget.component.ts
+++ frontend/project-herald/src/app/components/concert.widget/concert.widget.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Concert } from '../model/concert.model';
 
 @Component({
-  selector: 'app-concert.widget',
+  selector: 'app-concert-widget',
   standalone: true,
   imports: [],
   templateUrl: './concert.widget.component.html',
   styleUrl: './concert.widget.component.css'
 })
 export class ConcertWidgetComponent {
-
+  constructor() {}
+  @Input() concert: Concert | undefined;
 }
--- frontend/project-herald/src/app/components/main.page/main.page.component.ts
+++ frontend/project-herald/src/app/components/main.page/main.page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CONCERTS } from '../model/concert-list';
+import { ConcertWidgetComponent } from '../concert.widget/concert.widget.component';
 
 @Component({
   selector: 'app-herald',
   standalone: true,
-  imports: [],
+  imports: [ConcertWidgetComponent],
   templateUrl: './main.page.component.html',
   styleUrl: './main.page.component.css'
 })
 export class MainPageComponent {
-
+  concerts = CONCERTS;
 }
