d79f1f7414ba05f1f7c185fb1c8855f297640fa4
halkszavu <halkszavu@outlook.com>
2024-11-10 09:42:15 +0100

    add concert list model with sample data

---
+++ frontend/project-herald/src/app/components/model/concert-list.ts
@@ -0,0 +1,7 @@
+import { Concert } from "./concert.model";
+
+
+export const CONCERTS: Concert[] = [
+    new Concert('1', 'Karácsonyi koncert', '2020-01-01'),
+    new Concert('2', 'Újévi koncert', '2020-01-02'),
+];
