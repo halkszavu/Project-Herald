18fb5caa42f531069abbbd4c9eacd8a857acf954
halkszavu <halkszavu@outlook.com>
2024-11-10 09:23:01 +0100

    concert widget generated

---
+++ frontend/project-herald/src/app/components/concert.widget/concert.widget.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-concert.widget',
+  standalone: true,
+  imports: [],
+  templateUrl: './concert.widget.component.html',
+  styleUrl: './concert.widget.component.css'
+})
+export class ConcertWidgetComponent {
+
+}
