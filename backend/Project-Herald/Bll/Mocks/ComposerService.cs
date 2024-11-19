c3c19656c42afbfa5c53ac5d4820097a54ea97ea
halkszavu <halkszavu@outlook.com>
2024-11-19 14:33:31 +0100

    Mocking Composers

---
+++ backend/Project-Herald/Bll/Mocks/ComposerService.cs
@@ -0,0 +1,23 @@
+﻿using Bll.Services;
+using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bll.Mocks
+{
+	public class ComposerService : IComposerService
+	{
+		public Task<Composer> CreateComposerAsync(Composer period)
+		{
+			return Task.FromResult(new Composer());
+		}
+
+		public Task<Composer> UpdateComposerAsync(Composer period)
+		{
+			return Task.FromResult(new Composer());
+		}
+	}
+}
