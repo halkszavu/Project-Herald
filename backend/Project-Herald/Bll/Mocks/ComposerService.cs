006d65c5ef75cbf5641291a75fe56508516b0ec8
halkszavu <halkszavu@outlook.com>
2024-11-29 15:36:24 +0100

    expand services

--- backend/Project-Herald/Bll/Mocks/ComposerService.cs
+++ backend/Project-Herald/Bll/Mocks/ComposerService.cs
@@ -12,12 +12,23 @@ namespace Bll.Mocks
 	{
 		public Task<Composer> CreateComposerAsync(Composer period)
 		{
-			return Task.FromResult(new Composer());
+			return Task.FromResult(new Composer()
+			{
+				ID = "mockComposer",
+				Name = "Composer 1",
+				DateOfBirth = new DateTime(1900, 1, 1),
+				Period = new Period()
+				{
+					ID = "mockPeriod",
+					Name = "Period 1" 
+				},
+				Description = "Description 1"
+			});
 		}
 
 		public Task<Composer> UpdateComposerAsync(Composer period)
 		{
-			return Task.FromResult(new Composer());
+			return Task.FromResult(period);
 		}
 	}
 }
--- backend/Project-Herald/Bll/Mocks/PieceService.cs
+++ backend/Project-Herald/Bll/Mocks/PieceService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bll.Services;
+using DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace Bll.Mocks
 {
-	public class PieceService
+	public class PieceService : IPieceService
 	{
+		public Task<Piece> CreatePieceAsync(Piece period)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Piece> UpdatePieceAsync(Piece period)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
