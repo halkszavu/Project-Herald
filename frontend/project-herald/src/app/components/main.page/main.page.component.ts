df7f86335bb47ac3dad4996e8a642f2cad0e8516
halkszavu <halkszavu@outlook.com>
2024-12-02 23:39:19 +0100

    sidebar added to the "sidebar"

--- frontend/project-herald/src/app/components/main.page/main.page.component.ts
+++ frontend/project-herald/src/app/components/main.page/main.page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CONCERTS } from '../model/concert-list';
 import { ConcertWidgetComponent } from '../concert.widget/concert.widget.component';
+import { SidebarComponent } from "../sidebar/sidebar.component";
 
 @Component({
   selector: 'app-herald',
   standalone: true,
-  imports: [ConcertWidgetComponent],
+  imports: [ConcertWidgetComponent, SidebarComponent],
   templateUrl: './main.page.component.html',
   styleUrl: './main.page.component.css'
 })
