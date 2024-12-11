fd4674bf2b90b9312dc5ccec81523c2005fa7fb9
halkszavu <halkszavu@outlook.com>
2024-12-11 17:46:28 +0100

    add navigation component and integrate into main page; refactor sidebar component

--- frontend/project-herald/src/app/components/main.page/main.page.component.ts
+++ frontend/project-herald/src/app/components/main.page/main.page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CONCERTS } from '../model/concert-list';
 import { ConcertWidgetComponent } from '../concert.widget/concert.widget.component';
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { NavigationComponent } from "../navigation/navigation.component";
 
 @Component({
   selector: 'app-herald',
   standalone: true,
-  imports: [ConcertWidgetComponent, SidebarComponent],
+  imports: [ConcertWidgetComponent, SidebarComponent, NavigationComponent],
   templateUrl: './main.page.component.html',
   styleUrl: './main.page.component.css'
 })
--- frontend/project-herald/src/app/components/sidebar/sidebar.component.ts
+++ frontend/project-herald/src/app/components/sidebar/sidebar.component.ts
@@ -1,41 +1,10 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import {MatNativeDateModule, NativeDateAdapter} from '@angular/material/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  providers: [NativeDateAdapter],
-  imports: [MatDatepickerModule, MatFormFieldModule, MatNativeDateModule, FormsModule],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
-  picker: any;
-
-  genres = [
-    {name: 'Opera', selected: false},
-    {name: 'Symphony', selected: false},
-    {name: 'Concerto', selected: false},
-  ]
-
-  periods = [
-    {name: 'Baroque', selected: false},
-    {name: 'Classical', selected: false},
-    {name: 'Romantic', selected: false},
-    {name: 'Late Romantic', selected: false},
-    {name: '20th Century', selected: false},
-    {name: '21st Century', selected: false},
-  ]
-
-  onGenreCheckboxChange(event: any, genre: any){
-    genre.selected = event.target.checked;
-  }
-
-  onPeriodCheckboxChange(event: any, period: any){
-    period.selected = event.target.checked;
-  }
-}
+export class SidebarComponent { }
