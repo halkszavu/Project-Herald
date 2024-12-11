ae0ee9fe742e10e06f168ee285b9f85c3eae7131
halkszavu <halkszavu@outlook.com>
2024-12-11 13:02:31 +0100

    add genre and period selection checkboxes to sidebar component

--- frontend/project-herald/src/app/components/sidebar/sidebar.component.ts
+++ frontend/project-herald/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {MatNativeDateModule, NativeDateAdapter} from '@angular/material/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -7,11 +8,34 @@ import {MatFormFieldModule} from '@angular/material/form-field';
   selector: 'app-sidebar',
   standalone: true,
   providers: [NativeDateAdapter],
-  imports: [MatDatepickerModule, MatFormFieldModule, MatNativeDateModule],
+  imports: [MatDatepickerModule, MatFormFieldModule, MatNativeDateModule, FormsModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
   picker: any;
+
+  genres = [
+    {name: 'Opera', selected: false},
+    {name: 'Symphony', selected: false},
+    {name: 'Concerto', selected: false},
+  ]
+
+  periods = [
+    {name: 'Baroque', selected: false},
+    {name: 'Classical', selected: false},
+    {name: 'Romantic', selected: false},
+    {name: 'Late Romantic', selected: false},
+    {name: '20th Century', selected: false},
+    {name: '21st Century', selected: false},
+  ]
+
+  onGenreCheckboxChange(event: any, genre: any){
+    genre.selected = event.target.checked;
+  }
+
+  onPeriodCheckboxChange(event: any, period: any){
+    period.selected = event.target.checked;
+  }
 }
