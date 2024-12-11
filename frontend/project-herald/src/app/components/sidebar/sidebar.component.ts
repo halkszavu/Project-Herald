084adb132fbea3d78a58fb73da3f77751834675b
halkszavu <halkszavu@outlook.com>
2024-12-11 12:18:15 +0100

    enhance sidebar component with date range input and styling

--- frontend/project-herald/src/app/components/sidebar/sidebar.component.ts
+++ frontend/project-herald/src/app/components/sidebar/sidebar.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatNativeDateModule, NativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  providers: [NativeDateAdapter],
+  imports: [MatDatepickerModule, MatFormFieldModule, MatNativeDateModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
