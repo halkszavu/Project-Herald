6ee944b1df51c7c4036d4f77028a688695bc567f
halkszavu <halkszavu@outlook.com>
2024-12-11 20:48:49 +0100

    add new piece form component with template, styles, and tests

---
+++ frontend/project-herald/src/app/components/new-piece-form/new-piece-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-piece-form',
+  templateUrl: './new-piece-form.component.html',
+  styleUrl: './new-piece-form.component.css'
+})
+export class NewPieceFormComponent {
+
+}
