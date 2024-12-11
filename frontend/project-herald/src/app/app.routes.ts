507953d2a35f3d983c68299a1a0b1f81b40a3587
halkszavu <halkszavu@outlook.com>
2024-12-11 17:29:18 +0100

    add login form component and update routing

--- frontend/project-herald/src/app/app.routes.ts
+++ frontend/project-herald/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './components/main.page/main.page.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 export const routes: Routes = [
-    {path: 'main', component: MainPageComponent},
+    { path: 'main', component: MainPageComponent},
+    { path: 'login', component: LoginFormComponent},
 ];
