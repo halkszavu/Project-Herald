e825cd144dc38086d5f7636aa551ced1fdf4108a
halkszavu <halkszavu@outlook.com>
2024-12-12 16:13:33 +0100

    Login hacked together

---
+++ backend/Project-Herald/WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LoginController : ControllerBase
+	{
+		[HttpPost]
+		public IActionResult Post([FromBody] LoginRequest request)
+		{
+			if(request.Username == "admin" && request.Password == "admin")
+			{
+				return Ok(new LoginResponse { Token = "token" });
+			}
+			else
+			{
+				return Unauthorized();
+			}
+		}
+	}
+
+	public class LoginRequest
+	{
+		public string Username { get; internal set; }
+		public string Password { get; internal set; }
+	}
+
+	public class LoginResponse
+	{
+		public string Token { get; internal set; }
+	}
+}
